25c3db6d4afae3d4c1f93c7c071c2a77226223c6
Rob Kososki <robkososki@me.com>
2021-06-06 10:37:57 -0400

    Circuit one complete

--- assets/js/days.js
+++ assets/js/days.js
@@ -1,17 +1,10 @@
 const circOne = document.querySelector(".circ-one");
 const circTwo = document.querySelector(".circ-two");
 const circThree = document.querySelector(".circ-three");
-// const thursday = document.querySelector(".thu");
-// const friday = document.querySelector(".fri");
-// const saturday = document.querySelector(".sat");
-// const sunday = document.querySelector(".sun");
 const currentCircuit = document.querySelector(".routine");
 const prevSet = document.querySelector(".previous-set");
 const nextSet = document.querySelector(".next-set");
-// const circOneBtn = document.querySelector(".circuit-one");
-// const circTwoBtn = document.querySelector(".circuit-two");
-// const circThreeBtn = document.querySelector(".circuit-three");
-// remove the set nav buttons until the day is clicked
+
 function clearNav() {
     prevSet.style.display = "none";
     nextSet.style.display = "none";
@@ -28,13 +21,14 @@ function showNav() {
     prevSet.style.display = "inline";
     nextSet.style.display = "inline";
 }
-function hideNext(){
+
+function hideNext() {
     nextSet.style.display = "none";
 }
-function hidePrev(){
+
+function hidePrev() {
     prevSet.style.display = "none";
 }
-
 //MONDAY
 const circOneWorkoutOne = `<div class="examp-mon">
 <h2 class="align-center">Chest & Triceps</h2>
@@ -147,13 +141,158 @@ const circOneWorkoutThree = `<div class="examp-mon">
 </div>
 </div>
 `;
+const circOneWorkoutFour = `<div class="examp-mon">
+<h2 class="align-center">Chest & Triceps</h2>
+</div>
+<div>
+<div class ="align-center">
+    <h3 class = "index">Set Four</div>
+<div>
+</div>
+<div class="row">
+<div class="column">
+    <div class="left-column">
+        <p>
+            Bench Press
+            <br>
+            Hanging Leg Raises
+            <br>
+            Standing Tricep Extensions
+            <br>
+            Jumping Jacks
+        </p>
+    </div>
+</div>
+<div class="column">
+    <div class="right-column">
+        <p>
+            3 Reps at 180 lbs
+            <br>
+            20 Reps
+            <br>
+            3 reps at 50 lbs
+            <br>
+            50
+        </p>
+    </div>
+</div>
+</div>
+`;
+const circOneWorkoutFive = `<div class="examp-mon">
+<h2 class="align-center">Chest & Triceps</h2>
+</div>
+<div>
+<div class ="align-center">
+    <h3 class = "index">Set Five</div>
+<div>
+</div>
+<div class="row">
+<div class="column">
+    <div class="left-column">
+        <p>
+            Bench Press
+            <br>
+            Hanging Leg Raises
+            <br>
+            Standing Tricep Extensions
+            <br>
+            Jumping Jacks
+        </p>
+    </div>
+</div>
+<div class="column">
+    <div class="right-column">
+        <p>
+            6 Reps at 170 lbs
+            <br>
+            20 Reps
+            <br>
+            8 reps at 45 lbs
+            <br>
+            50
+        </p>
+    </div>
+</div>
+</div>
+`;
+const circOneWorkoutSix = `<div class="examp-mon">
+<h2 class="align-center">Chest & Triceps</h2>
+</div>
+<div>
+<div class ="align-center">
+    <h3 class = "index">Set Six</div>
+<div>
+</div>
+<div class="row">
+<div class="column">
+    <div class="left-column">
+        <p>
+            Bench Press
+            <br>
+            Hanging Leg Raises
+            <br>
+            Standing Tricep Extensions
+            <br>
+            Jumping Jacks
+        </p>
+    </div>
+</div>
+<div class="column">
+    <div class="right-column">
+        <p>
+            10 Reps at 155 lbs
+            <br>
+            15 Reps
+            <br>
+            10 reps at 40 lbs
+            <br>
+            50
+        </p>
+    </div>
+</div>
+</div>
+`;
+const circOneWorkoutSeven = `<div class="examp-mon">
+<h2 class="align-center">Chest & Triceps</h2>
+</div>
+<div>
+<div class ="align-center">
+    <h3 class = "index">Set Seven</div>
+<div>
+</div>
+<div class="row">
+<div class="column">
+    <div class="left-column">
+        <p>
+            Bench Press
+            <br>
+            Hanging Leg Raises
+            <br>
+            Standing Tricep Extensions
+            <br>
+            Jumping Jacks
+        </p>
+    </div>
+</div>
+<div class="column">
+    <div class="right-column">
+        <p>
+            12 Reps at 140 lbs
+            <br>
+            10 Reps
+            <br>
+            12 reps at 35 lbs
+            <br>
+            50
+        </p>
+    </div>
+</div>
+</div>
+`;
 const circTwoWorkoutOne = "circuit two";
 const circThreeWorkoutOne = "circuit three";
-
-
 let circNum = 0;
 let x = 0;
-
 
 function displayCircOneWorkout() {
     currentCircuit.innerHTML = circOneWorkoutOne;
@@ -166,7 +305,7 @@ function displayCircOneWorkout() {
 function displayCircTwoWorkout() {
     currentCircuit.innerHTML = circTwoWorkoutOne;
     showNav()
-    // x = 2;
+    x = 8;
     circNum = 2;
     console.log(x);
 }
@@ -178,26 +317,10 @@ function displayCircThreeWorkout() {
     circNum = 3;
     console.log(x);
 }
-
-
 circOne.addEventListener("click", displayCircOneWorkout)
 circTwo.addEventListener("click", displayCircTwoWorkout)
 circThree.addEventListener("click", displayCircThreeWorkout)
-// thursday.addEventListener("click", displayThuWorkout)
-// friday.addEventListener("click", displayFriWorkout)
-// saturday.addEventListener("click", displaySatWorkout)
-// sunday.addEventListener("click", displaySunWorkout)
-//Nav Buttons
-// function createBtn() {
-//     let prevBtn = document.createElement("button");
-//     prevBtn.innerHTML ="Previous Circuit"
-//     prevBtn.classList.add = 'previous-button';
-//     document.body.appendChild(prevBtn);
-//     let nextBtn = document.createElement("button");
-//     nextBtn.innerHTML = "Next Circuit";
-//     nextBtn.className = 'next-button';
-//     document.body.appendChild(nextBtn);
-// }
+
 function funNextSet() {
     switch (x) {
         case 1:
@@ -207,6 +330,26 @@ function funNextSet() {
             break;
         case 2:
             currentCircuit.innerHTML = circOneWorkoutThree;
+            x += 1;
+            console.log(x);
+            break;
+        case 3:
+            currentCircuit.innerHTML = circOneWorkoutFour;
+            x += 1;
+            console.log(x);
+            break;
+        case 4:
+            currentCircuit.innerHTML = circOneWorkoutFive;
+            x += 1;
+            console.log(x);
+            break;
+        case 5:
+            currentCircuit.innerHTML = circOneWorkoutSix;
+            x += 1;
+            console.log(x);
+            break;
+        case 6:
+            currentCircuit.innerHTML = circOneWorkoutSeven;
             x += 1;
             console.log(x);
             break;
@@ -220,38 +363,38 @@ function funPrevSet() {
             currentCircuit.innerHTML = circOneWorkoutOne;
             console.log(x);
             break;
-
         case 2:
             x -= 1;
             currentCircuit.innerHTML = circOneWorkoutOne;
             console.log(x);
-
             break;
         case 3:
             x -= 1;
             currentCircuit.innerHTML = circOneWorkoutTwo;
             console.log(x);
-
+            break;
+        case 4:
+            x -= 1;
+            currentCircuit.innerHTML = circOneWorkoutThree;
+            console.log(x);
+            break;
+        case 5:
+            x -= 1;
+            currentCircuit.innerHTML = circOneWorkoutFour;
+            console.log(x);
+            break;
+        case 6:
+            x -= 1;
+            currentCircuit.innerHTML = circOneWorkoutFive;
+            console.log(x);
+            break;
+        case 7:
+            x -= 1;
+            currentCircuit.innerHTML = circOneWorkoutSix;
+            console.log(x);
             break;
     }
 };
-
 nextSet.addEventListener("click", funNextSet);
 prevSet.addEventListener("click", funPrevSet);
 console.log(x);
-
-// Circuit Switch
-// function switchCircuit() {
-//     currentCircuit.innerHTML = "";
-// }
-// circOne = circOneBtn.addEventListener("click", switchCircuit);
-// circTwo = circTwoBtn.addEventListener("click", switchCircuit);
-// circThree = circThreeBtn.addEventListener("click", switchCircuit);
-
-
-// index each set then have the fucntion check for that index number, if it's 2 then next will be 3 and previous will be one
-//If the next button is clicked then incriment a variable by 1 and if previous is clicked decriment by 1, you can then check 
-//what the number is, and write if/else statements
-//or add something hidden to each displayed workout that you can check for, if there, you will know which one
-//to display when next button or previous button is clicked
-//If monday Circiut one is clicked, display that.
